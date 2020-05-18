8f44e0747a245efd6a513a5c254acde20c6d56f2
GEnAdInE <one.robin@outlook.fr>
2020-05-18 12:41:41 +0200

    diff nico

--- src/com/sulfuro/Main.java
+++ src/com/sulfuro/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("fopakzefkaez");
+        System.out.println("Robin");
+        System.out.println("caca");
     }
 }
