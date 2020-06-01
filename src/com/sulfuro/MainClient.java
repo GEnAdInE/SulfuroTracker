2b3b0a823b19c30ea566774c574d278c2d808445
GEnAdInE <one.robin@outlook.fr>
2020-06-01 16:36:04 +0200

    Improved time 5.0

--- src/com/sulfuro/MainClient.java
+++ src/com/sulfuro/MainClient.java
@@ -14,14 +14,17 @@ public class MainClient {
 
 
 
-        com.sulfuro.model.Time t0 = new Time(23,00);
+        com.sulfuro.model.Time t0 = new Time(00,00);
        com.sulfuro.model.Time  t1 = new Time(10,00);
        Time t3;
+       Time t4;
 
         System.out.println(Time.TimeToString(t0));
         System.out.println(Time.TimeToString(t1));
-        t3 = Time.Addition(t0,t1);
+        t3 = Time.Substraction(t0,t1);
         System.out.println(Time.TimeToString(t3));
+        t4 = Time.Substraction(t3,t3);
+        System.out.println(Time.TimeToString(t4));
 
     }
 }
